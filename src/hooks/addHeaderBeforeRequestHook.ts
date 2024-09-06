ed978e0ed797313fd481e09fe7f461862d8429ae
Sayan kar <sayan.kar@avalabs.org>
2024-09-06 21:40:54 +0530

    Update Hook

--- src/hooks/addHeaderBeforeRequestHook.ts
+++ src/hooks/addHeaderBeforeRequestHook.ts
@@ -2,15 +2,13 @@ import {
   BeforeRequestContext,
   BeforeRequestHook,
 } from "./types";
-import { version } from "../../package.json";
-
 
 export class AddHeaderBeforeRequestHook
   implements BeforeRequestHook
 { 
   beforeRequest(_hookCtx: BeforeRequestContext, request: Request): Request {
     // modify the request object before it is sent, such as adding headers or query parameters, or throw an error to stop the request from being sent
-    request.headers.set('X-App-Component', `avacloud-sdk/${version}`);
+    request.headers.set('X-App-Component', `avacloud-sdk`);
     return request;
   }
 }
